893cb44cf409d4cc3610786386401dea64324ae5
ilia_solovev <ilia_solovev@epam.com>
2021-03-02 19:03:49 +0400

    Удалил Main class

--- src/main/java/calculator/Main.java
+++
@@ -1,9 +0,0 @@
-package calculator;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.sum(2, 2));
-    }
-}
